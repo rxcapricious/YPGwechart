48cca6918ff159e691d80d8c979795d52d98111f
renxinmei <renxinmei@wakedata.com>
2020-11-26 20:30:26 +0800

    fix:页面上拉加载、下拉刷新

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,4 +1,28 @@
-// pages/goods_list/index.js
+/* 1、用户上滑  触底加载下一页
+      1、找到滚动条触底事件
+      2、判断下一页有无数据
+         1、获取总页数
+          总页数 = Math.ceil(总条数 / 页容量pagesize)
+         2、获取到当前页码   pagenum
+         3、判断当前页码是否大于等于总页数
+      3、若无数据，就弹出提示框
+      4、有就继续加载
+         1、当前页码 ++
+         2、重新发送请求
+         3、数据请求回来  需要将数组进行拼接
+
+2、下拉刷新页面
+   1、触发下拉刷新事件  需要在页面的json文件中开启配置项
+   2、重置  数据 数组
+   3、重置页面  设置为1
+   4、重新发送请求
+   5、数据请求回来需要手动关闭等待效果
+
+*/
+
+import { request } from "../../request/index.js";
+import regeneratorRuntime from '../../lib/runtime/runtime';
+
 Page({
    /**
    * 页面的初始数据
@@ -20,14 +44,39 @@ Page({
         value: "综合",
         isActive: false,
       }
-    ]
+    ],
+    goodsList: [],
   },
+
+  // 接口要的参数
+  QueryParams: {
+    query: '',
+    cid: '',
+    pagenum: 1,
+    pagesize: 10
+  },
+  totalPages:1,
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.QueryParams.cid = options.cid;
+    this.getGoodsList();
+  },
 
+  // 获取商品列表数据
+  async getGoodsList() {
+    const res = await request({url:"/goods/search",data:this.QueryParams});
+    // 获取总条数
+    const total = res.total;
+    this.totalPages = Math.ceil(total/this.QueryParams.pagesize);
+    this.setData({
+      goodsList:[...this.data.goodsList,...res.goods]
+    })
+
+    // 关闭下拉刷新接口
+    wx.stopPullDownRefresh();
   },
 
   // 标题的点击事件  从子组件传递过来
@@ -43,5 +92,31 @@ Page({
     this.setData({
       tabs
     })
+  },
+
+  // 页面滚动条触底事件
+  onReachBottom() {
+    // 1、判断还有没有下一页
+    if(this.QueryParams.pagenum>=this.totalPages) {
+      // 没有下一页数据
+      wx.showToast({
+        title: '没有下一页数据',
+      });
+    }else {
+      this.QueryParams.pagenum++;
+      this.getGoodsList();
+    }
+  },
+
+  // 下拉刷新事件
+  onPullDownRefresh() {
+    // 1、重置数组
+    this.setData({
+      goodsList:[]
+    })
+    // 更改页码
+    this.QueryParams.pagenum=1;
+    // 3、发送请求
+    this.getGoodsList();
   }
 })
--- request/index.js
+++ request/index.js
@@ -1,4 +1,13 @@
+
+// 同时发送异步代码的次数
+let ajaxTime=0;
 export const request=(params)=> {
+    ajaxTime++;
+    // 显示加载中的效果
+    wx.showLoading({
+        title: '加载中',
+        mask:true
+    });
     // 定义公共的urL
     const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1";
     return new Promise((resolve,reject) => {
@@ -10,6 +19,13 @@ export const request=(params)=> {
             },
             fail:(err)=> {
                 reject(err);
+            },
+            complete:()=> {
+                ajaxTime--;
+                // 关闭正在等待的图标
+                if(ajaxTime === 0) {
+                    wx.hideLoading();
+                }
             }
         })
     })
